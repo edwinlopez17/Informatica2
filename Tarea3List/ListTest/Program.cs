d075c45d2a16cfcb1450ad3c26e61b3b993e5c48
edwinlopez17 <lopez171073@unis.edu.gt>
2018-05-15 22:16:15 -0600

    Agregando Tareas Faltantes

---
+++ Tarea3List/ListTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Programas;
+using System;
+namespace ListTest
+{
+    [TestClass]
+    public class ListTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            IList<int> lista = new ArrayList<int>(new int[] { 10, 30 });
+            lista.Push(50);
+            Assert.AreEqual(lista.Length, 3);
+            int last;
+            lista.Get(lista.Length - 1, out last);
+            Assert.AreEqual(last, 50);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            IList<int> lista = new ArrayList<int>(new int[] { 10, 30 });
+            Assert.AreEqual(lista.Get(30, out int obj), false);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            IList<int> lista = new ArrayList<int>(new int[] { 10, 30 });
+            lista.Set(1, 50);
+            int obj;
+            lista.Get(1, out obj);
+            Assert.AreEqual(obj, 50);
+        }
+    }
+}
